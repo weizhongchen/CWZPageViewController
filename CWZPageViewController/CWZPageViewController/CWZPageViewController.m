cd94ed2976dce1beaaec408f816bc65c51b9ded7
陈为忠 <479448326@qq.com>
2018-10-18 16:31:53 +0800

    33

--- CWZPageViewController/CWZPageViewController/CWZPageViewController.m
+++ CWZPageViewController/CWZPageViewController/CWZPageViewController.m
@@ -8,12 +8,7 @@
 
 #import "CWZPageViewController.h"
 #import "CWZSelectView.h"
-//22
-
-//44
-
 //33
-
 @interface CWZPageViewController ()<UIScrollViewDelegate>
 @property(nonatomic,strong)UIScrollView*scrollV;
 @property(nonatomic,strong)NSArray*titls;
