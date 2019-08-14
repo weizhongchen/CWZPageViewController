596c71142506b2c58608e4d01a9653eee42d7915
chenweizhong <weizhong_c@cpass.com>
2019-08-14 09:33:00 +0800

    master分支修改

--- CWZPageViewController/CWZPageViewController/AppDelegate.m
+++ CWZPageViewController/CWZPageViewController/AppDelegate.m
@@ -17,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    //master分支修改
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     [self.window makeKeyAndVisible];
