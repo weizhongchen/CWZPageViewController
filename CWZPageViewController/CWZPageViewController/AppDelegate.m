facf0d07e7f2b1f27fa85fd9219f785e2cfd3ffe
chenweizhong <weizhong_c@cpass.com>
2019-09-25 13:22:38 +0800

    Master

--- CWZPageViewController/CWZPageViewController/AppDelegate.m
+++ CWZPageViewController/CWZPageViewController/AppDelegate.m
@@ -96,6 +96,5 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
-
-
+//master
 @end
